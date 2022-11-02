ee0ee328d3aa624fc53aed1a6b39051a85327142
Max Shchur <mxm.shchur@gmail.com>
2022-11-02 13:24:41 +0200

    First steps in creating collection oj jokes

---
+++ shutki.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+
+
+def insert_into_file(final_result):
+    file = open("minutka_yumora.txt", "w+", encoding='utf-8')
+    file.write(final_result)
+    file.close()
+
+
+def get_jokes_about_shtirliz():
+    url = 'https://anekdoty.ru/pro-shtirlica/'
+    page = requests.get(url)
+    data = page.text
+
+    for_editing = ' Минутка юмора \n \n'
+    final_result = f'{for_editing}'
+
+    soup = BeautifulSoup(data, 'html.parser')
+    joke_body = soup.find_all('div', class_='holder-body')
+    for jokes in joke_body:
+        joke = jokes.text
+        final_result += joke.replace('\n', '')
+
+        final_result += f'\n \n {for_editing}'
+
+    insert_into_file(final_result)
+
+
+root = Tk()
+root.title("JOKES")
+root.geometry("550x450")
+icon = ImageTk.PhotoImage(Image.open("icons/icon_smile.jpg"))
+
+# Create a Label Widget to display the text or Image
+root.iconphoto(False, icon)
+
+shtirliz_jokes_btn = ttk.Button(text="Анекдоты про Штирлица", command=get_jokes_about_shtirliz)
+shtirliz_jokes_btn.place(relx=0.1, rely=0.1, anchor='nw', relwidth=0.35, relheight=0.15)
+
+
+root.mainloop()
+
+
