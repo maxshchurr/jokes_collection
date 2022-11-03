67c1d2222a5835f99dd2c70f2008937e7d4ad304
Max Shchur <mxm.shchur@gmail.com>
2022-11-03 21:33:49 +0200

    extended parser to run through a pages, small fixes with visual content

--- shutki.py
+++ shutki.py
@@ -12,60 +12,79 @@ def insert_into_file(final_result):
     file.close()
 
 
-def get_jokes_about_shtirliz():
-    url = 'https://anekdoty.ru/pro-shtirlica/'
-    page = requests.get(url)
-    data = page.text
+class Shtirliz:
 
-    for_editing = ' Минутка юмора \n \n'
-    final_result = f'{for_editing}'
+    @staticmethod
+    def get_jokes_about_shtirliz():
 
-    soup = BeautifulSoup(data, 'html.parser')
-    joke_body = soup.find_all('div', class_='holder-body')
-    for jokes in joke_body:
-        joke = jokes.text
-        final_result += joke.replace('\n', '')
+        for_editing = ' Минутка юмора \n \n'
+        final_result = f'{for_editing}'
 
-        final_result += f'\n \n {for_editing}'
+        for page_number in range(1, 13):
+            url = f'https://anekdoty.ru/pro-shtirlica/page/{page_number}/'
+            page = requests.get(url)
+            data = page.text
+            soup = BeautifulSoup(data, 'html.parser')
+            joke_body = soup.find_all('div', class_='holder-body')
 
-    insert_into_file(final_result)
+            for jokes in joke_body:
+                joke = jokes.text
+                final_result += joke.replace('\n', '')
 
+                final_result += f'\n \n {for_editing}'
 
-def get_jokes_about_programmers():
-    url = 'https://anekdoty.ru/pro-programmistov/'
-
-    page = requests.get(url)
-    data = page.text
-
-    for_editing = ' Минутка юмора \n \n'
-    final_result = f'{for_editing}'
-
-    soup = BeautifulSoup(data, 'html.parser')
-    joke_body = soup.find_all('div', class_='holder-body')
-    for jokes in joke_body:
-        joke = jokes.text
-        final_result += joke.replace('\n', '')
-
-        final_result += f'\n \n {for_editing}'
-
-    insert_into_file(final_result)
+        insert_into_file(final_result)
 
 
-root = Tk()
-root.title("JOKES")
-root.geometry("550x450")
-icon = ImageTk.PhotoImage(Image.open("icons/icon_smile.jpg"))
+class Programmers:
 
-# Create a Label Widget to display the text or Image
-root.iconphoto(False, icon)
+    @staticmethod
+    def get_jokes_about_programmers():
 
-shtirliz_jokes_btn = ttk.Button(text="Анекдоты про Штирлица", command=get_jokes_about_shtirliz)
-shtirliz_jokes_btn.place(relx=0.1, rely=0.1, anchor='nw', relwidth=0.35, relheight=0.15)
+        for_editing = ' Минутка юмора \n \n'
+        final_result = f'{for_editing}'
 
-programmers_jokes_btn = ttk.Button(text="Анекдоты про программистов", command=get_jokes_about_programmers)
-programmers_jokes_btn.place(relx=0.1, rely=0.4, anchor='w', relwidth=0.35, relheight=0.15)
+        for page_number in range(1, 13):
+            url = f'https://anekdoty.ru/pro-programmistov/page/{page_number}/'
+            page = requests.get(url)
+            data = page.text
+            soup = BeautifulSoup(data, 'html.parser')
+            joke_body = soup.find_all('div', class_='holder-body')
+
+            for jokes in joke_body:
+                joke = jokes.text
+                final_result += joke.replace('\n', '')
+                final_result += f'\n \n {for_editing}'
+
+                print(joke)
+
+            # for future changes
+            # list = root.grid_slaves()
+            # for l in list:
+            #     l.destroy()
+
+        insert_into_file(final_result)
 
 
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    root.title("JOKES")
+    root.geometry("550x450")
+    icon = ImageTk.PhotoImage(Image.open("icons/icon_smile.jpg"))
+
+    # Create a Label Widget to display the text or Image
+    root.iconphoto(False, icon)
+
+    shtirliz = Shtirliz()
+    shtirliz_jokes_btn = ttk.Button(text="Анекдоты про Штирлица",
+                                    command=shtirliz.get_jokes_about_shtirliz).grid(
+                                    row=0, column=0, sticky=NW, pady=5, padx=10)
+
+    programmers = Programmers()
+    programmers_jokes_btn = ttk.Button(text="Анекдоты про программистов",
+                                       command=Programmers.get_jokes_about_programmers).grid(
+                                       row=1, column=0, sticky=W, pady=5, padx=10)
+
+    root.mainloop()
 
 
